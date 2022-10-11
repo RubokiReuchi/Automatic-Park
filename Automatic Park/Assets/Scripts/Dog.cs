df800abaea0896ddb830676f6ecb7b9dad705dc6
Ruboki <ruboki.reuchi@gmail.com>
2022-10-11 09:50:28 +0200

    dog script

--- Automatic Park/Assets/Scripts/Dog.cs
+++ Automatic Park/Assets/Scripts/Dog.cs
@@ -3,40 +3,99 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum state_machine
+{
+    FOLLOW,
+    FLEE,
+    IDLE,
+    RETURN,
+}
+
 public class Dog : MonoBehaviour
 {
+    public GameObject runner;
+    [SerializeField] state_machine state;
     NavMeshAgent agent;
-    bool state; // false --> idle, true --> flee
-    Transform target;
+    public Transform[] points;
+    int point_selected;
+    public float max_speed;
+    public float acceleration;
+    public float time_idle;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        state = state_machine.FOLLOW;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (state)
+        switch (state)
         {
-            if (Vector3.Distance(transform.position, target.position) <= 1)
-            {
-                state = false;
-            }
-            else
-            {
-                agent.SetDestination(target.position);
-            }
+            case state_machine.FOLLOW:
+                agent.speed = max_speed;
+                agent.SetDestination(runner.transform.position);
+                agent.acceleration = acceleration;
+                break;
+            case state_machine.FLEE:
+                agent.SetDestination(points[point_selected].position);
+                if (in_water)
+                {
+                    agent.speed = max_speed * 0.75f;
+                    agent.acceleration = acceleration * 0.75f;
+                }
+                else
+                {
+                    agent.speed = max_speed * 2;
+                    agent.acceleration = acceleration * 2;
+                }
+
+                if (Vector3.Distance(this.transform.position, points[point_selected].position) <= 2)
+                {
+                    state = state_machine.IDLE;
+                    StartCoroutine(Rest());
+                }
+                break;
+            case state_machine.IDLE:
+                agent.speed = 0.0f;
+                break;
+            case state_machine.RETURN:
+                agent.SetDestination(runner.transform.position);
+                if (in_water)
+                {
+                    agent.speed = max_speed * 0.75f;
+                    agent.acceleration = acceleration * 0.75f;
+                }
+                else
+                {
+                    agent.speed = max_speed * 2;
+                    agent.acceleration = acceleration * 2;
+                }
+
+                if (Vector3.Distance(this.transform.position, runner.transform.position) <= 2)
+                {
+                    state = state_machine.FOLLOW;
+                }
+                break;
+            default:
+                break;
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Bee" && state == false)
+        if (other.CompareTag("bee") && state == state_machine.FOLLOW)
         {
-            state = true;
-            target.position = new Vector3(Random.Range(-5, 5), 1, Random.Range(-5, 5));
+            state = state_machine.FLEE;
+            point_selected = Random.Range(0, points.Length);
         }
+    }
+
+    IEnumerator Rest()
+    {
+        yield return new WaitForSeconds(time_idle);
+        state = state_machine.RETURN;
     }
 }
